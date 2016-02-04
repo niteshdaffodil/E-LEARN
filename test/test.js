e0b29e649d63a70dcbf9bab718a2efa21b809ee0
= <=>
2016-02-04 19:19:51 +0530

    test

---
+++ test/test.js
@@ -0,0 +1,25 @@
+var assert = require('chai').assert;
+
+describe("Response Handler",function(){
+	var helper = require('../helper/response');
+	it("should return object with length three",function(done){
+		assert.equal(Object.keys(helper.responseObject()).length,3);
+		done();
+	});
+	it("should return object with status code 200",function(done){
+		assert.equal(helper.responseObject().statusCode,200);
+		done();
+	});
+	it("should return object with message is ok",function(done){
+		assert.equal(helper.responseObject().message,"ok");
+		done();
+	});
+	it("should return object with status code 200 if we pass only one argument with 200",function(done){
+		assert.equal(helper.responseObject(200).statusCode,200);
+		done();
+	});
+	it("should return object with message is ok if we pass second argument ok",function(done){
+		assert.equal(helper.responseObject(null, "ok").message,"ok");
+		done();
+	});
+});
