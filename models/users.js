92e28330c397f2fa6a76e06ddcc586898a0c2e55
= <=>
2016-02-06 20:31:56 +0530

    add mongoose-dateonly npm package

--- models/users.js
+++ models/users.js
@@ -1,12 +1,13 @@
 var timestamps = require('mongoose-timestamp');
 var mongoose = require('mongoose');
+var DateOnly = require('mongoose-dateonly')(mongoose);
 var Schema = mongoose.Schema;
 
 var userSchema = Schema({
   email: { type: String, required: true, unique: true },
   password : { type: String, required: true },
   name : { type: String, required: true },
-  dob : { type: Date, required: true },
+  dob : { type: DateOnly, required: true },
   lastlogin_at : { type: Date },
   avatar_url : { type: String },
   ambassdor : { type: Boolean, default: false },
