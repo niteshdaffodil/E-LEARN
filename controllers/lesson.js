5cc98e9efb7651b9c6b5dc443111e8472616446e
= <=>
2016-02-06 15:35:27 +0530

    lesson api

--- controllers/lesson.js
+++ controllers/lesson.js
@@ -2,13 +2,16 @@ var Lesson = require('../models/lessons');
 var helper = require('../helper/response');
 module.exports = {
 	addLesson : function(req, res, next){
-		Lesson.findOne({name:req.body.name,unit:req.body.unit})
+		Lesson.findOne({name:req.body.name,module:req.body.module})
 		.then(function(lesson){
 			if(!lesson){
-				(new Lesson(req.body)).save()
-				.then(function(lesson){
-					req.result = lesson;
-					next();
+				(new Lesson(req.body)).save(function(err, lesson){
+					if(err){
+						res.json(helper.responseObject(422, err, null, true));
+					}else {
+						req.result = lesson;
+						next();				
+					}
 				})
 			}else{
 				throw({message:"lesson name must be unique"});
@@ -17,5 +20,25 @@ module.exports = {
 		.catch(function(err){
 			res.json(helper.responseObject(422, err.message, null));
 		});
+	},
+	getAllLesson : function(req, res, next){
+		Lesson.find({deleted:false})
+		.then(function(lessons){
+			req.result = lessons;
+			next();
+		})
+		.catch(function(err){
+			res.json(helper.responseObject(422, err, null, true));
+		})
+	},
+	getLesson : function(req, res, next){
+		Lesson.find({_id:req.params.id,deleted:false})
+		.then(function(lesson){
+			req.result = lesson;
+			next();
+		})
+		.catch(function(err){
+			res.json(helper.responseObject(422, err, null, true));
+		})
 	}
 }
--- routes/lessons.js
+++ routes/lessons.js
@@ -5,5 +5,7 @@ var helper = require('../helper/response');
 var router = express.Router();
 /* GET users listing. */
 router.post('/',[lessonController.addLesson,helper.handleSuccess]);
+router.get('/',[lessonController.getAllLesson,helper.handleSuccess]);
+router.get('/:id',[lessonController.getLesson,helper.handleSuccess]);
 
 module.exports = router;
