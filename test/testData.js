092bc42af5d95fe877aefeb084d67cde9e58327f
= <=>
2016-02-08 14:42:42 +0530

    testcases formodule API route :modules/:id

---
+++ test/testData.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'module' : {
+		"_id" : "56b5a4df0bb35f19244f1724",
+		"name" : "module 8",
+		"description" : "descrtion of module eight",
+		"deleted" : false,
+		"__v" : 0
+	}
+}
